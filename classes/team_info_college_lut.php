abb49919085ae21e9301086be68f2f3c97b74116
Peter Law <PeterJCLaw@gmail.com>
2013-12-07 23:44:03 +0000

    Check for failure when decoding teams.json
    If teams.json fails to be decoded, then the website would emit various
    cascade errors into the webserver log. This patch adds a check that
    makes sure the LUT was successfully parsed. If it wasn't, it emits a
    custom error at that point so that it's clear what the root cause was.
    
    Change-Id: Ia2dbf958a454d12f7045eec913463b937065a88e

--- classes/team_info_college_lut.php
+++ classes/team_info_college_lut.php
@@ -3,3 +3,7 @@
 $teams_json = 'resources/2014/teams.json';
 
 $team_info_college_lut = json_decode(file_get_contents($teams_json), true);
+
+if ($team_info_college_lut === null) {
+	trigger_error("Failed to parse team LUT", E_USER_WARNING);
+}
